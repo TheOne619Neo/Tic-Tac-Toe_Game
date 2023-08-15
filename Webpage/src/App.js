1ebf8dbdce2bdbb36d38724f20cd8d0fbf0355ce
TheOne_Neo <nemohap022@student.wethinkcode.co.za>
2023-08-15 17:03:30 +0200

    name

---
+++ Webpage/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>JavaScript Master</code> Neo TheOne_ 
+          <p> </p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+        <a
+          className="App-link"
+          href="https://github.com/TheOne619Neo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Follow Me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
